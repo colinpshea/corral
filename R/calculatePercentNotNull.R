9eb0a6305e440e384b288cce4e8cc09fabd973ed
Shea <colin.shea@myfwc.com>
2025-07-24 08:46:26 -0400

    another change

--- R/calculatePercentNotNull.R
+++ R/calculatePercentNotNull.R
@@ -1,6 +1,6 @@
 #' Calculate percent not null
 #' @description This function calculates the percentage of columns (loci) that are not `NA` or `NULL` values i.e., that have valid `SNP` genetic data.
 #' @param x Any data frame, matrix, vector.
-#' @returns This function takes data (a vector, data frame, or matrix) and calculates the percentage of observations that are not `NA` or `NULL` values. This is used to calculate the percentage of loci with valid SNP genetic data.
+#' @returns This function takes data (a vector, data frame, or matrix) and calculates the percentage of observations that are not `NA` or `NULL` values. This is used to calculate the percentage of loci with valid SNP data.
 #' @export
 calcPercentNotNull <- function(x) 100 - mean(is.na(x)*100)
